b34af3767d90d6bab933eb07df906ce5a7417a06
Aaron <aabaker99@gmail.com>
2019-03-27 14:17:35 -0500

    update to write the centers to a file

--- script/cell_centers.m
+++ script/cell_centers.m
@@ -1,12 +1,14 @@
-function rv = cell_centers(fp)
-  arr = imread(fp);
+function rv = cell_centers(infile, outfile)
+  % todo switch to python
+  arr = imread(infile);
   bw = imbinarize(arr);
   [L, num] = bwlabel(bw);
-  rv = zeros(num);
+  rv = zeros(num,2);
   stats = regionprops(L, 'centroid');
   for i=1:num
     stats_struct = stats(i);
-    centroid = stats_struct('centroid')
-    rv(i) = centroid;
+    centroid = stats_struct.Centroid;
+    rv(i,:) = centroid;
   end
+  csvwrite(outfile, rv);
 end
