1219b3066cff9c9a5678c2362eecbfc87e696934
Aaron <aabaker99@gmail.com>
2019-04-18 11:13:09 -0500

    fix indexing, create images associated with the labels as well

--- script/cell_clustering.py
+++ script/cell_clustering.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
 import sys, argparse
+import os, os.path
 import numpy as np
 import sklearn.cluster
 import skimage.io
 import skimage.filters
+from skimage.color import rgb2gray
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--cell-centers', required=True, help="Cell centers found by connected components on nucleus channel")
   parser.add_argument('--image-file', required=True, help="Ph_golgi channel")
   parser.add_argument('--outfile', required=True, help="Matrix with the same dimensions as <image_file> with an integer cluster label in each cell which identifies which cluster the corresponding pixel belongs to.")
+  parser.add_argument('--outdir', required=True, help="Location to save segmented images")
   args = parser.parse_args()
 
   cell_centers = np.genfromtxt(args.cell_centers, delimiter=",")
   n_cells, n_dim = cell_centers.shape
 
+  # use MATLAB style for image indexes
   arr = skimage.io.imread(args.image_file)
-  arr = arr.transpose() # use MATLAB style for image indexes
+  if len(arr.shape) == 3:
+    # then image is rbg
+    arr = rgb2gray(arr)
 
   # read intensity at cell center, append it
   intensity = np.zeros((n_cells, 1))
   for i in range(n_cells):
     # adjust for Python origin 0 indexes versus Matlab origin 1 indexes
-    x = int(round(cell_centers[i,0])) - 1
-    y = int(round(cell_centers[i,1])) - 1
+    x = int(round(cell_centers[i,1])) - 1
+    y = int(round(cell_centers[i,0])) - 1
     intensity[i,0] = arr[x,y]
   cell_centers = np.concatenate((cell_centers, intensity), axis=1)
 
@@ -51,6 +57,12 @@ def main():
     i, j = k_to_ij[k]
     arr_labels[i,j] = kmeans.labels_[k]
   np.savetxt(args.outfile, arr_labels, delimiter=",", fmt='%d')
+
+  for i in range(n_cells):
+    fp = os.path.join(args.outdir, 'cell{}.png'.format(i))
+    arr_i = np.copy(arr)
+    arr_i[arr_labels != i] = 0
+    skimage.io.imsave(fp, arr_i)
 
 if __name__ == "__main__":
   main()
